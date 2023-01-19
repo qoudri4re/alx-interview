9075a9d056efaa66db3f8a569f9e1fe5f9808c4b
qoudri4re <qoudri4re@gmail.com>
2023-01-19 23:58:13 +0100

    --

--- 0x03-log_parsing/0-stats.py
+++ 0x03-log_parsing/0-stats.py
@@ -4,82 +4,35 @@
 import re
 
 
-def extract_input(input_line):
-    '''Extracts sections of a line of an HTTP request log.
-    '''
-    fp = (
-        r'\s*(?P<ip>\S+)\s*',
-        r'\s*\[(?P<date>\d+\-\d+\-\d+ \d+:\d+:\d+\.\d+)\]',
-        r'\s*"(?P<request>[^"]*)"\s*',
-        r'\s*(?P<status_code>\S+)',
-        r'\s*(?P<file_size>\d+)'
-    )
-    info = {
-        'status_code': 0,
-        'file_size': 0,
-    }
-    log_fmt = '{}\\-{}{}{}{}\\s*'.format(fp[0], fp[1], fp[2], fp[3], fp[4])
-    resp_match = re.fullmatch(log_fmt, input_line)
-    if resp_match is not None:
-        status_code = resp_match.group('status_code')
-        file_size = int(resp_match.group('file_size'))
-        info['status_code'] = status_code
-        info['file_size'] = file_size
-    return info
-
-
-def print_statistics(total_file_size, status_codes_stats):
-    '''Prints the accumulated statistics of the HTTP request log.
-    '''
-    print('File size: {:d}'.format(total_file_size), flush=True)
-    for status_code in sorted(status_codes_stats.keys()):
-        num = status_codes_stats.get(status_code, 0)
-        if num > 0:
-            print('{:s}: {:d}'.format(status_code, num), flush=True)
-
-
-def update_metrics(line, total_file_size, status_codes_stats):
-    '''Updates the metrics from a given HTTP request log.
-    Args:
-        line (str): The line of input from which to retrieve the metrics.
-    Returns:
-        int: The new total file size.
-    '''
-    line_info = extract_input(line)
-    status_code = line_info.get('status_code', '0')
-    if status_code in status_codes_stats.keys():
-        status_codes_stats[status_code] += 1
-    return total_file_size + line_info['file_size']
-
-
 def run():
-    '''Starts the log parser.
-    '''
-    line_num = 0
-    total_file_size = 0
-    status_codes_stats = {
-        '200': 0,
-        '301': 0,
-        '400': 0,
-        '401': 0,
-        '403': 0,
-        '404': 0,
-        '405': 0,
-        '500': 0,
-    }
-    try:
-        while True:
-            line = input()
-            total_file_size = update_metrics(
-                line,
-                total_file_size,
-                status_codes_stats,
-            )
-            line_num += 1
-            if line_num % 10 == 0:
-                print_statistics(total_file_size, status_codes_stats)
-    except (KeyboardInterrupt, EOFError):
-        print_statistics(total_file_size, status_codes_stats)
+    '''run'''
+    metrics = {"totalFileSize": 0, "statusCodes":
+               {200: 0, 301: 0, 400: 0, 401: 0, 403: 0, 404: 0, 405: 0, 500: 0}
+               }
+
+    entries = 0
+    interrupt = False
+    while entries < 10 and not interrupt:
+        try:
+            entry = input()
+            match = re.search(r'(?P<ip>\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}) - \[(?P<date>.*)\] ' +
+                              '"(?P<method>\w+) (?P<path>.*) (?P<protocol>HTTP/\d\.\d)" (?P<status>\d+) (?P<size>\d+)', entry)
+
+            if match:
+                metrics["totalFileSize"] += int(match.group("size"))
+                if int(match.group("status")) in metrics["statusCodes"]:
+                    metrics["statusCodes"][int(match.group("status"))] += 1
+                entries += 1
+
+        except (KeyboardInterrupt, EOFError):
+            entries = 10
+            interrupt = True
+    print("File size: ", metrics['totalFileSize'])
+    statusCodes = list(metrics["statusCodes"].keys())
+    statusCodes.sort()
+    for statusCode in statusCodes:
+        if metrics["statusCodes"][statusCode]:
+            print(statusCode, ": ", metrics["statusCodes"][statusCode])
 
 
 if __name__ == '__main__':
