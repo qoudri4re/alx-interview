eca3fcaf924884e0375fcc74c0aa8b89ad856bd5
qoudri4re <qoudri4re@gmail.com>
2023-01-26 21:14:25 +0100

    interview tasks

--- 0x04-utf8_validation/0-validate_utf8.py
+++ 0x04-utf8_validation/0-validate_utf8.py
@@ -1,36 +1,61 @@
 #!/usr/bin/python3
+"""UTF-8 validation module.
+"""
+
 
 def validUTF8(data):
-    num_of_bytes_to_process = 0
-    for num in data:
-        # get the binary representation of the current byte
-        binary_num = format(num, '#010b')[-8:]
-        if num_of_bytes_to_process == 0:
-            # if the first bit is 1, it's a continuation byte
-            if binary_num[0] == '1':
+    """Checks if a list of integers are valid UTF-8 codepoints.
+    See <https://datatracker.ietf.org/doc/html/rfc3629#page-4>
+    """
+    skip = 0
+    n = len(data)
+    for i in range(n):
+        if skip > 0:
+            skip -= 1
+            continue
+        if type(data[i]) != int or data[i] < 0 or data[i] > 0x10ffff:
+            return False
+        elif data[i] <= 0x7f:
+            skip = 0
+        elif data[i] & 0b11111000 == 0b11110000:
+            # 4-byte utf-8 character encoding
+            span = 4
+            if n - i >= span:
+                next_body = list(map(
+                    lambda x: x & 0b11000000 == 0b10000000,
+                    data[i + 1: i + span],
+                ))
+                if not all(next_body):
+                    return False
+                skip = span - 1
+            else:
                 return False
-            # if the first bit is 0, it's a single-byte character
-            elif binary_num[0] == '0':
-                continue
-            # if the first three bits are 110, it's the start of a two-byte character
-            elif binary_num[:3] == '110':
-                num_of_bytes_to_process = 1
-            # if the first four bits are 1110, it's the start of a three-byte character
-            elif binary_num[:4] == '1110':
-                num_of_bytes_to_process = 2
-            # if the first five bits are 11110, it's the start of a four-byte character
-            elif binary_num[:5] == '11110':
-                num_of_bytes_to_process = 3
-            # if none of the above, it's not a valid UTF-8 character
+        elif data[i] & 0b11110000 == 0b11100000:
+            # 3-byte utf-8 character encoding
+            span = 3
+            if n - i >= span:
+                next_body = list(map(
+                    lambda x: x & 0b11000000 == 0b10000000,
+                    data[i + 1: i + span],
+                ))
+                if not all(next_body):
+                    return False
+                skip = span - 1
+            else:
+                return False
+        elif data[i] & 0b11100000 == 0b11000000:
+            # 2-byte utf-8 character encoding
+            span = 2
+            if n - i >= span:
+                next_body = list(map(
+                    lambda x: x & 0b11000000 == 0b10000000,
+                    data[i + 1: i + span],
+                ))
+                if not all(next_body):
+                    return False
+                skip = span - 1
             else:
                 return False
         else:
-            # if the first two bits are not 10, it's not a valid continuation byte
-            if binary_num[:2] != '10':
-                return False
-            num_of_bytes_to_process -= 1
-    # if we've processed all bytes and there are no remaining bytes to process, it's a valid UTF-8 string
-    if num_of_bytes_to_process == 0:
-        return True
-    else:
-        return False
+            return False
+    return True
