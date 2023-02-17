8fd99aa0293c7d057a3ffe53dc8000954214dba0
qoudri4re <qoudri4re@gmail.com>
2023-02-17 06:38:55 +0100

    ---

---
+++ 0x07-rotate_2d_matrix/0-rotate_2d_matrix.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+"""2D matrix rotation module.
+"""
+
+def rotate_2d_matrix(matrix):
+    """rotates an array"""
+    n = len(matrix)
+    # Transpose the matrix (i.e. swap rows and columns)
+    for i in range(n):
+        for j in range(i, n):
+            matrix[i][j], matrix[j][i] = matrix[j][i], matrix[i][j]
+    # Reverse each row of the transposed matrix
+    for i in range(n):
+        matrix[i] = matrix[i][::-1]
